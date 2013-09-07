7540d058620c343e056d04e995a1f225f92cb301
Trey Griffith <trey.griffith@gmail.com>
2013-09-07 15:11:37 -0700

    add skeleton of single page app

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var Filequeue = require('filequeue');
+
+var fq = new Filequeue();
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+
+	fq.readFile(__dirname + '/views/index.html', {encoding: 'utf8'}, function(err, str) {
+
+		if(err) {
+			res.send(err);
+			return;
+		}
+
+		res.send(str);
+	});
+});
+
+app.listen(process.env.PORT || 3000);
